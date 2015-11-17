1d2759fe2937f29f02d837748ba26e7a31f7ee59
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2015-11-16 21:54:24 -0800

    pleasework

---
+++ public/btix2/app/nflTeam/nflTeam_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.nflTeam module', function() {
+
+  beforeEach(module('myApp.nflTeam'));
+
+  describe('view1 controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('NflTeamCtrl');
+      expect(nflTeamCtrl).toBeDefined();
+    }));
+
+  });
+});
